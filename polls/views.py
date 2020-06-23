5397a6240b8dd045182f62a25c5b64c4a421f8d6
David Folk <davidwfolk@gmail.com>
2020-06-23 10:49:21 -0600

    setting up tests v2

--- polls/views.py
+++ polls/views.py
@@ -11,6 +11,8 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
+#future posts
+from django.utils import timezone
 
 # from .models import Question
 from .models import Choice, Question
@@ -69,6 +71,12 @@ class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 # def results(request, question_id):
 #     # return HttpResponse("You're looking at the results of %s." % question_id)
 #     question = get_object_or_404(Question, pk=question_id)
@@ -101,3 +109,12 @@ def vote(request, question_id):
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question': question})
+
+def get_queryset(self):
+    """
+    Return the last five published questions (not including those set to be
+    published in the future).
+    """
+    return Question.objects.filter(
+        pub_date__lte=timezone.now()
+    ).order_by('-pub_date')[:5]
